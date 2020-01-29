9ba1dcac7ff6cb716202e1811129423840c904cd
Ekdahl, Anders <anders.ekdahl@if.se>
2020-01-29 14:52:02 +0100

    updated search for einstein

--- cypress/integration/MyTests/find-Einstein.js
+++ cypress/integration/MyTests/find-Einstein.js
@@ -10,6 +10,6 @@ describe('Finding Einstein', function () {
 	it('on Google', function() {
 		cy.visit('https://www.google.com/search?hl=en')
 		cy.get('input[name="q"]').type('Einstein').type('{downarrow}').type('{enter}')
-		cy.get('#res #search #rso .r a h3').find('span').first().should('contain', 'Albert')
+		cy.get('#search #rso div.g').first().should('contain', 'Albert')
 	})
 })
